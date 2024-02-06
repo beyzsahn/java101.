fc165f12a1eb985451ce2f2a81e61aee52cddea9
beyza <75841191+beyzsahn@users.noreply.github.com>
2024-02-06 23:57:56 +0300

    Add files via upload

---
+++ deseneGöreMetot.java
@@ -0,0 +1,31 @@
+package com.example.Java101;
+import java.util.Scanner;
+public class deseneGöreMetot {
+    public static void islemYap(int sayi) {
+        System.out.print(sayi + " ");
+        if (sayi <= 0)
+        {
+            return;
+        }
+
+
+
+        if (sayi > 0)
+        {
+            islemYap(sayi - 5);
+            System.out.print(sayi + " ");
+        }
+
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir sayı girin: ");
+        int n = scanner.nextInt();
+
+        islemYap(n);
+    }
+
+
+}
