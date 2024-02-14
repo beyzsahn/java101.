25bb1ab31dbff13ffb53ad4c1dd33295ae311715
beyza <75841191+beyzsahn@users.noreply.github.com>
2024-02-14 21:34:23 +0300

    Add files via upload

---
+++ palindromikKelimeler.java
@@ -0,0 +1,21 @@
+package com.example.Java101;
+import java.util.Scanner;
+
+public class palindromikKelimeler {
+    static boolean isPalindrome(String str){
+        int i=0;
+        int j=str.length()-1;
+        while (i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        System.out.println(isPalindrome("hüseyin"));
+    }
+}
